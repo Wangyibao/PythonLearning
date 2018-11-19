ab07cf137d40917e0ea90d8d154f83a5ea9c27b0
lihaogn <lihaogn@163.com>
2018-11-19 22:01:45 +0800

    python input output

---
+++ python-learn01.py
@@ -0,0 +1,14 @@
+# 输出
+print('hello world');
+print('hello','world','hello','python');
+print(300);
+print(100+300);
+print('100+200= ',100+200);
+
+# 输入
+name=input(); # 输入任意字符，按回车完成输入
+print('name= ',name);
+
+name=input('please enter your name:')
+print('name= ',name);
+
