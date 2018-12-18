a044576944997e0b8ac2500733df61018ad422eb
wangyi <1307441720@qq.com>
2018-12-18 20:32:44 +0800

    review 12/18

---
+++ review 02 输入与输出.py
@@ -0,0 +1,12 @@
+# 1 输出
+print('hello world');
+print('hello','world','hello','python');# 拼成一句话，遇到逗号输出一个空格
+print(300)
+print(100+200); # 输出:300
+print('100+200=',100+200);
+
+# 2 输入
+name=input(); # 输入任意字符，按回车完成输入
+print('name=',name); #打印出刚刚输入的内容
+
+name=input('please enter your name:'); # 添加提示信息
